53c73c30fc422ff2511a0324b18c8f383eb84750
Harsh Thakur <meharshthakur.3365@gmail.com>
2025-05-27 18:38:18 +0530

    done

---
+++ A_Extremely_Round.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << (s.size() - 1)*9 + (s[0] - '0') << endl;
+    }
+}
---
+++ A_Two_Permutations.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, a, b;
+        cin >> n >> a >> b;
+        if((a == b) && (n == a)){
+            cout << "Yes" << endl;
+        }
+        else if((a + b + 2) <= n){
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+}
